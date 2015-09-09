bdbc70ef1b342f1f713b34c658be0fc9f817b7f6
nkhuong <phannkhuong@yahoo.com>
2015-09-08 22:43:05 -0400

    Initial Commit

---
+++ GPS Tracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPS Tracking
+//
+//  Created by Khuong Phan Nhat on 9/8/15.
+//  Copyright (c) 2015 Skypatrol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
